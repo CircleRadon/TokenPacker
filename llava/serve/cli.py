62cd035a5e0b0ca89b1d8935ec7ab55bb0203549
LI Wentong <sunshine_lwt@126.com>
2024-08-28 00:57:24 +0800

    update cli.py

--- llava/serve/cli.py
+++ llava/serve/cli.py
@@ -1,5 +1,6 @@
 import argparse
 import torch
+import os
 
 from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
 from llava.conversation import conv_templates, SeparatorStyle
@@ -25,8 +26,8 @@ import time
 def main(args):
     # Model
     disable_torch_init()
-
-    model_name = get_model_name_from_path(args.model_path)
+    model_path = os.path.expanduser(args.model_path)
+    model_name = get_model_name_from_path(model_path)
     tokenizer = AutoTokenizer.from_pretrained(
         args.model_path,
         model_max_length = 2048,
@@ -47,7 +48,8 @@ def main(args):
     vision_tower.to(device='cuda', dtype=torch.float16)
     image_processor = vision_tower.image_processor
 
-    image_patch = Image_Patch(patch_num=16)
+    patch_num = getattr(model.config, 'patch_num', '9')
+    image_patch = Image_Patch(int(patch_num))
     preprocess = Compose([ToTensor(), Normalize((0.48145466, 0.4578275, 0.40821073),(0.26862954, 0.26130258, 0.27577711))])
 
     
@@ -61,44 +63,49 @@ def main(args):
         image_file = input("image file: ")
 
         image = Image.open(image_file).convert('RGB')
-        image = preprocess(image)
-        ## add
-        image = image.unsqueeze(0)
-        h, w = image.shape[-2:]
-        block_size = 336
-        h_block, w_block = image_patch.calculate(h, w)
-        h_ratio = block_size*h_block/h
-        w_ratio = block_size*w_block/w
-        if h_ratio<=w_ratio:
-            w_ = min(block_size*w_block, round(w*h_ratio))
-            h_ = block_size*h_block
-        else:
-            w_ = block_size*w_block
-            h_ = min(block_size*h_block, round(h*w_ratio))
-        image_inter = F.interpolate(image, size=(h_,w_), mode='bilinear')
-        image = torch.zeros((1, 3, block_size*h_block, block_size*w_block)).to(dtype=image_inter.dtype, device=image_inter.device)
-        image[:, :, :h_, :w_] = image_inter
 
-        split_images = []
-        for i_ in range(h_block):
-            for j_ in range(w_block):
-                image_s = image[:,:,block_size*i_:block_size*(i_+1), block_size*j_:block_size*(j_+1)]
-                split_images.append(image_s)
-        if len(split_images)>1:
-            h_ratio = block_size/h
-            w_ratio = block_size/w
+        if model.config.image_aspect_ratio == 'slice':
+            image = preprocess(image)
+            image = image.unsqueeze(0)
+            h, w = image.shape[-2:]
+            block_size = 336
+            h_block, w_block = image_patch.calculate(h, w)
+            h_ratio = block_size*h_block/h
+            w_ratio = block_size*w_block/w
             if h_ratio<=w_ratio:
-                w_ = min(block_size, round(w*h_ratio))
-                h_ = block_size
+                w_ = min(block_size*w_block, round(w*h_ratio))
+                h_ = block_size*h_block
             else:
-                w_ = block_size
-                h_ = min(block_size, round(h*w_ratio))
+                w_ = block_size*w_block
+                h_ = min(block_size*h_block, round(h*w_ratio))
             image_inter = F.interpolate(image, size=(h_,w_), mode='bilinear')
-            image_s = torch.zeros((1, 3, block_size, block_size)).to(dtype=image_inter.dtype, device=image_inter.device)
-            image_s[:, :, :h_, :w_] = image_inter
-            split_images.append(image_s)
-        image_tensor = torch.cat(split_images, dim=0)
+            image = torch.zeros((1, 3, block_size*h_block, block_size*w_block)).to(dtype=image_inter.dtype, device=image_inter.device)
+            image[:, :, :h_, :w_] = image_inter
 
+            split_images = []
+            for i_ in range(h_block):
+                for j_ in range(w_block):
+                    image_s = image[:,:,block_size*i_:block_size*(i_+1), block_size*j_:block_size*(j_+1)]
+                    split_images.append(image_s)
+            if len(split_images)>1:
+                h_ratio = block_size/h
+                w_ratio = block_size/w
+                if h_ratio<=w_ratio:
+                    w_ = min(block_size, round(w*h_ratio))
+                    h_ = block_size
+                else:
+                    w_ = block_size
+                    h_ = min(block_size, round(h*w_ratio))
+                image_inter = F.interpolate(image, size=(h_,w_), mode='bilinear')
+                image_s = torch.zeros((1, 3, block_size, block_size)).to(dtype=image_inter.dtype, device=image_inter.device)
+                image_s[:, :, :h_, :w_] = image_inter
+                split_images.append(image_s)
+            image_tensor = torch.cat(split_images, dim=0)
+        else:
+            image_tensor = process_images([image], image_processor, model.config)[0]
+            image_tensor = image_tensor.unsqueeze(0)
+            h_block = 1
+            w_block = 1
 
         try:
             inp = input(f"{roles[0]}: ")
@@ -130,17 +137,18 @@ def main(args):
         stopping_criteria = KeywordsStoppingCriteria(keywords, tokenizer, input_ids)
         streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
 
+        mode = model.config.image_aspect_ratio
         with torch.inference_mode():
             model.orig_forward = model.forward
             model.forward = partial(model.orig_forward,
-                                    h_block=[h_block],
-                                    w_block=[w_block],
-                                    bs_patch_num=[len(image_tensor)])
+                                    mode=mode,
+                                    h_block=h_block,
+                                    w_block=w_block)
             start = time.time()
 
             output_ids = model.generate(
                 input_ids,
-                images=image_tensor.bfloat16().cuda(),
+                images=image_tensor.to(dtype=torch.bfloat16, device='cuda', non_blocking=True),
                 do_sample=True,
                 temperature=args.temperature,
                 max_new_tokens=args.max_new_tokens,
@@ -160,9 +168,7 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model-path", type=str, default="tokenpaker-7b")
-    parser.add_argument("--model-base", type=str, default=None)
-    parser.add_argument("--image-file", type=str)
+    parser.add_argument("--model-path", type=str, default="path/to/tokenpacker")
     parser.add_argument("--device", type=str, default="cuda")
     parser.add_argument("--conv-mode", type=str, default='vicuna_v1')
     parser.add_argument("--temperature", type=float, default=0.2)
@@ -170,6 +176,5 @@ if __name__ == "__main__":
     parser.add_argument("--load-8bit", action="store_true")
     parser.add_argument("--load-4bit", action="store_true")
     parser.add_argument("--debug", action="store_true")
-    parser.add_argument("--image-aspect-ratio", type=str, default='pad')
     args = parser.parse_args()
     main(args)
